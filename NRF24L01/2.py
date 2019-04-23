5bd695edf6c5d79d2ac2c75d4f4c4d2295563854
jslimaye <2016.jalaj.limaye@ves.ac.in>
2019-04-23 11:42:50 +0530

    2 arduinos

--- NRF24L01/2.py
+++ NRF24L01/2.py
@@ -24,24 +24,27 @@ radio.printDetails()
 
 def change_params(radio):
     radio.stopListening()
-    radio.closeReadingPipe()
-    radio.openReadingPipe(1, pipes[0])
+    #radio.closeReadingPipe(0)
+    #radio.openReadingPipe(1, pipes[0])
     radio.setChannel(0x70)
     radio.startListening()
     print(2)
-    sleep(2)
+#    sleep(2)
 
 
 def revert_params(radio):
     radio.stopListening()
-    radio.closeReadingPipe()
-    radio.openReadingPipe(1, pipes[1])
+    #radio.closeReadingPipe(1,pipes[0])
+    #radio.openReadingPipe(1, pipes[1])
     radio.setChannel(0x64)
     radio.startListening()
     print(1)
-    sleep(2)
+    #sleep(1)
 
 def takeData(radio):
+    while not radio.available(0):
+        sleep(1/100)
+#        print("waiting")
     receivedMessage = []
     radio.read(receivedMessage, radio.getDynamicPayloadSize())
     print("Received: {}".format(receivedMessage))
@@ -53,8 +56,9 @@ def takeData(radio):
         if (n>=32 and n<= 126):
             string += chr(n)
     print("Out received message decodes to: {}".format(string))
-    air = int(string[:3])
-    temp = float(string[4:9])
+    var = string.split('#')
+    air = var[0]
+    temp = var[1]
 
     print("AireQua=",air," temp=",temp)
     connection(temp,air)
@@ -64,9 +68,6 @@ def takeData(radio):
 
 while(1):
 # ackPL = [1]
-    while not radio.available(0):
-        sleep(1/100)
-        print("waiting")
         
     change_params(radio)
     sleep(1)
